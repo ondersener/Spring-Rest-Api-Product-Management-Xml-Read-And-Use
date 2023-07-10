143d87eef8098fe832269260a06024631c514a00
ondersener <int.ondersener@gmail.com>
2023-07-11 00:44:52 +0300

    Undo

--- src/main/java/com/works/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.works.repositories;
-
-import com.works.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
